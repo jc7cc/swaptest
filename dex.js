74f499f258b3e76964eb8727bd2ba25ed3575feb
zhe sun <sunzhe1996@gmail.com>
2023-01-05 16:21:32 +0800

    get price from panckae

---
+++ dex.js
@@ -0,0 +1,67 @@
+import Web3 from "web3";
+import { routerv2ABI } from "./routerV2";
+
+export const web3 = new Web3("http://176.9.92.6:8000");
+
+const routerAddress = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
+const router = new web3.eth.Contract(routerv2ABI, routerAddress);
+
+const status = {
+  success: "success",
+  fail: "fail",
+};
+
+const token = {
+  WBNB: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+  BUSD: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+};
+
+const type = {
+  buy: "buy",
+  sell: "sell",
+};
+
+async function getAmountsOut(amountsIn, path) {
+  try {
+    const res = await router.methods.getAmountsOut(amountsIn, path).call();
+    return {
+      status: status.success,
+      amountIn: res[0],
+      amountOut: res[1],
+      price: +(+web3.utils.fromWei(res[0]) / +web3.utils.fromWei(res[1]))
+        .toFixed(2),
+    };
+  } catch (err) {
+    return {
+      status: status.fail,
+    };
+  }
+}
+
+async function getAmountsIn(amountsOut, path) {
+  try {
+    const res = await router.methods.getAmountsIn(amountsOut, path).call();
+    return {
+      status: status.success,
+      amountIn: res[0],
+      amountOut: res[1],
+      price: +(+web3.utils.fromWei(res[1]) / +web3.utils.fromWei(res[0]))
+        .toFixed(2),
+    };
+  } catch (err) {
+    return {
+      status: status.fail,
+    };
+  }
+}
+
+async function getBNBPrice(swapType) {
+  switch (swapType) {
+    case type.buy:
+      return await getAmountsIn("0.1", [BUSD, WBNB]);
+    case type.sell:
+      return await getAmountsOut("0.1", [WBNB, BUSD]);
+  }
+}
+
+getBNBPrice().then(console.log);
---
+++ env.js
@@ -0,0 +1,8 @@
+import Web3 from "web3";
+const web3 = new Web3();
+const privateKey = process.argv[2];
+const account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+export const env = {
+  account: account,
+};
