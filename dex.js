f9dbae61a601e46dfa82d6766dab0f514ff1d4ff
zhe sun <sunzhe1996@gmail.com>
2023-01-08 16:21:15 +0800

    test apeswap

--- dex.js
+++ dex.js
@@ -9,7 +9,7 @@ import fs from "fs";
 export const web3 = new Web3("http://176.9.92.6:8000");
 
 // const routerAddress = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
-const routerAddress = "0x3a6d8cA21D1CF76F653A67577FA0D27453350dD8";
+const routerAddress = "0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7";
 const router = new web3.eth.Contract(routerv2ABI, routerAddress);
 
 const status = {
